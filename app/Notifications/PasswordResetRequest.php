2f15a1066d204c45e635c295b3252a0145f36ca6
Vishal Bhandare <vbhandare@webuy.com>
2020-05-04 01:21:33 +0530

    Changes to be committed: 	modified:   app/Notifications/PasswordResetRequest.php 	modified:   config/app.php 	modified:   config/mail.php

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -42,7 +42,7 @@ class PasswordResetRequest extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
-        $url = 'http://localhost:3000/resetpassword?token='.$this->token;
+        $url = config('app.url').'/resetpassword?token='.$this->token;
         return (new MailMessage)
         ->line('You are receiving this email because we received a password reset request for your account.')
         ->action('Reset Password', url($url))
--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Appointment App'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://obscure-gorge-40760.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
